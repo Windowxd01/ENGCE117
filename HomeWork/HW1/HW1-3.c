60a2ffb73baad813796448ee4dc8a8ea160e2912
Windowxd01 <133748311+Windowxd01@users.noreply.github.com>
2024-11-26 21:14:22 +0700

    Upload

---
+++ HomeWork/HW1/HW1-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char firstname [101];
+    char lastname [101];
+
+    printf( "First Name : " ) ;
+    scanf( "%s", &firstname ) ;
+    printf( "Last Name : " ) ;
+    scanf( "%s", &lastname ) ;
+
+    printf( " %s", firstname ) ;
+    printf( " %ss ", lastname ) ;
+    printf( "TC, " ) ;
+    printf( "RMUTL, " ) ;
+    printf( "Chiang Mai, " ) ;
+    printf( "Thailand " );
+
+}
