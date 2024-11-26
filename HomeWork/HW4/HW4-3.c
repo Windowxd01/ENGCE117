cabc4197dd55463a8659c3984c1f7042cdcc3810
Windowxd01 <133748311+Windowxd01@users.noreply.github.com>
2024-11-26 22:52:20 +0700

    Upload

--- HomeWork/HW4/HW4-3.c
+++ HomeWork/HW4/HW4-3.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    
+    int number1 ;
+    int number2 ;
+    int number3 ;
+
+    scanf( "%d %d %d", &number1, &number2, &number3 );
+
+    printf( "Answer = %d", number1 + number2 + number3 );
 }
